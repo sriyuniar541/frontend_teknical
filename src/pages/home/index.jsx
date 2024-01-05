a841af65e4844da0983d3b87d7ad8ae87d1de412
sri yuniar <sriyuniar541@gmail.com>
2024-01-05 13:09:36 +0900

    update

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,8 +5,9 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import ExportToExcel from "../../components/excel";
 
+
 export const Home = () => {
-  const [data, setData] = useState([]);
+  const [tableData, setTableData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
@@ -19,37 +20,34 @@ export const Home = () => {
     try {
       const token = localStorage.getItem("token");
 
-      const response = await axios.get(
-        `${process.env.REACT_APP_URL_BE}/analytic/click`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-          params: {
-            start_date: startDate.toISOString().split("T")[0],
-            end_date: endDate.toISOString().split("T")[0],
-          },
-        }
-      );
+      let apiUrl = `${process.env.REACT_APP_URL_BE}/analytic/click`;
 
-      setData(response.data);
+      // Check if startDate and endDate are provided
+      if (startDate && endDate) {
+        apiUrl += `?start_date=${
+          startDate.toISOString().split("T")[0]
+        }&end_date=${endDate.toISOString().split("T")[0]}`;
+      }
+
+      const response = await axios.get(apiUrl, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setTableData(response.data);
       setLoading(false);
-      
     } catch (error) {
       console.error("Failed to fetch data:", error);
-      setData([]);
+      setTableData([]);
       setLoading(false);
-      
     }
   };
 
   return (
     <div className="container">
-      <div className="d-lg-flex justify-content-between my-3 ">
-        <div>
-          <LogoutButton />
-        </div>
-
+      <div className="d-lg-flex justify-content-between my-3">
+        <LogoutButton />
         <div>
           <label>Start Date: </label>
           <DatePicker
@@ -58,41 +56,34 @@ export const Home = () => {
             className="form-control"
           />
         </div>
-
         <div>
           <label>End Date: </label>
           <DatePicker
             selected={endDate}
             onChange={(date) => setEndDate(date)}
-            className="form-control "
+            className="form-control"
           />
         </div>
-        <button
-          className="ms-lg-3 btn btn-white border-primary"
-          onClick={fetchData}
-        >
+        <button onClick={fetchData} className="btn btn-white border-primary">
           Fetch Data
         </button>
       </div>
 
       {loading ? (
         <p>Loading...</p>
-      ) : data.length ? (
-        <table
-          className="table table-bordered table-hover text-center"
-          id="table-to-export"
-        >
+      ) : tableData.length ? (
+        <table className="table table-bordered text-center " id="table-to-export">
           <thead>
-            <tr className="table-secondary">
+            <tr className="table-light">
               <th scope="col">#</th>
               <th scope="col">Scope</th>
               <th scope="col">Count</th>
             </tr>
           </thead>
           <tbody>
-            {data.map((item, index) => (
+            {tableData.map((item, index) => (
               <tr key={index}>
-                <td scope="row">{index + 1}</td>
+                <th scope="row">{index + 1}</th>
                 <td>{item.scope}</td>
                 <td>{item.count}</td>
               </tr>
